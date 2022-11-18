788edd7de1a5f0e9371033e242f64598f61759f3
Kaspar Vakkum <apelsin.kv@gmail.com>
2022-11-18 10:48:08 +0200

    A

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
 
+$host = "d110077.mysql.zonevs.eu";
+$db = "d110077_todo";
+$user = "d110077sa421004";
+$passwd = "";
+
+$dsn = "mysql:host=$host;dbname=$db";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try{
+    $pdo = new PDO($dsn, $user, $passwd);
+} catch (\PDOException $e){
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+die();
+
 class Task {
     public $description;
     protected $is_completed = false;
