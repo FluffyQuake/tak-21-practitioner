689e8164744a1dbcb04abc430fcb6c600ab6cbc9
Kaspar Vakkum <apelsin.kv@gmail.com>
2022-11-30 11:27:42 +0200

    Latest

---
+++ bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+$config = require_once('config.php');
+
+require_once('database/Connection.php');
+require_once('database/QueryBuilder.php');
+
+return new QueryBuilder(
+    Connection::make($config['database'])
+);
