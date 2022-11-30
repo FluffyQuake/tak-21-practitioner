26d0ddb4ebed8d4317f5e21841eff9f72ccb1149
Kaspar Vakkum <apelsin.kv@gmail.com>
2022-11-30 09:04:07 +0200

    Fucntion

--- functions.php
+++ functions.php
@@ -1,9 +1,5 @@
 <?php
 function connectToDb () {
-    $host = "d110077.mysql.zonevs.eu";
-    $db = "d110077_todo";
-    $user = "d110077sa421004";
-    $passwd = "Bustywomen1,";
     $charset ="utf8mb4";
 
     $dsn = "mysql:host=$host;dbname=$db";
