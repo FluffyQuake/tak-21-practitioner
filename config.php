fd7ba43729bf21225a01b1f14a3116353de0e143
Kaspar Vakkum <apelsin.kv@gmail.com>
2023-01-16 11:43:57 +0200

    A

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 
 return[
     'database' => [ 
-        'host' => 'd110077.mysql.zonevs.eu',
-        'db' => 'd110077_todo',
-        'user' => 'd110077sa421004',
-        'passwd' => 'Bustywomen1,',
+        'host' => '',
+        'db' => '',
+        'user' => '',
+        'passwd' => ',',
         'charset' => 'utf8mb4',
         'options' => []
     ]
